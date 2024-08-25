9a958b5c50237c01d240a15c359462177a59f201
Muhammad Hasnat <mhasnat647@gmail.com>
2024-08-26 02:15:52 +0500

    updated the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,8 +30,8 @@ const socialData = [
 function Footer() {
   return (
     <>
-      <footer className="py-16 bg-veryDarkBlue">
-        <div className=" container flex flex-col items-center justify-between mx-auto space-y-16 px-6 md:flex-row md:space-y-0">
+      <footer className="py-14 bg-veryDarkBlue">
+        <div className=" container flex flex-col items-center justify-between md:space-x-10 mx-auto space-y-16 px-1 md:flex-row md:space-y-0">
           <div className="flex flex-col items-center justify-between space-y-8 text-lg font-light md:flex-row md:space-y-0 md:space-x-14 text-grayishBlue">
             <img src={logoBookmark} alt="" />
             {menu.map((item, index) => (
@@ -51,7 +51,17 @@ function Footer() {
               </a>
             ))}
           </div>
+          <a
+            href="https://www.linkedin.com/in/hasnatamjad"
+            target="_blank"
+            className="text-grayishBlue text-sm hover:text-softRed"
+          >
+            Developed by Hasnat
+          </a>
         </div>
+        {/* <div className="container flex items-center justify-center md:justify-end mx-auto px-6">
+          <p className="text-grayishBlue text-sm">Developed by Hasnat</p>
+        </div> */}
       </footer>
     </>
   );
